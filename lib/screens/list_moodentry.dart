8362e8eb22632ad0135964ca5034ddd68a0eb372
VicoAritonang <chaikaaaaa5@gmail.com>
2024-11-20 09:00:08 +0700

    tutor c

--- lib/screens/list_moodentry.dart
+++ lib/screens/list_moodentry.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:mental_health_tracker/models/mood_entry.dart';
-
+import 'package:mental_health_tracker/screens/menu.dart';
+import 'package:flutter/material.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';  
+import 'package:provider/provider.dart';
+import 'package:mental_health_tracker/screens/register.dart';
 import 'package:mental_health_tracker/widgets/left_drawer.dart';
 
 class MoodEntryPage extends StatefulWidget {
@@ -13,7 +17,7 @@ class MoodEntryPage extends StatefulWidget {
 class _MoodEntryPageState extends State<MoodEntryPage> {
   Future<List<MoodEntry>> fetchMood(CookieRequest request) async {
     // TODO: Ganti URL dan jangan lupa tambahkan trailing slash (/) di akhir URL!
-    final response = await request.get('http://[APP_URL_KAMU]/json/');
+    final response = await request.get('http://127.0.0.1:8000/json/');
     
     // Melakukan decode response menjadi bentuk json
     var data = response;
--- lib/widgets/mood_card.dart
+++ lib/widgets/mood_card.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:mental_health_tracker/screens/login.dart';
 import 'package:mental_health_tracker/screens/moodentry_form.dart';
 import 'package:mental_health_tracker/screens/menu.dart'; 
-import 'package:mental_health_tracker/screens/list_moodentry.dart'; // Pastikan import yang diperlukan
+import 'package:mental_health_tracker/screens/list_moodentry.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart'; // Pastikan import yang diperlukan
 
 class MoodCard extends StatelessWidget {
   final ItemHomepage item; // Pastikan item dideklarasikan
@@ -10,11 +13,12 @@ class MoodCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final request = context.watch<CookieRequest>();
     return Material(
       color: Theme.of(context).colorScheme.secondary,
       borderRadius: BorderRadius.circular(12),
       child: InkWell(
-        onTap: () {
+        onTap: () async {
           ScaffoldMessenger.of(context)
             ..hideCurrentSnackBar()
             ..showSnackBar(SnackBar(
@@ -31,6 +35,30 @@ class MoodCard extends StatelessWidget {
             ),
     );
 }
+          else if (item.name == "Logout") {
+              final response = await request.logout(
+                  // TODO: Ganti URL dan jangan lupa tambahkan trailing slash (/) di akhir URL!
+                  "http://127.0.0.1:8000/auth/logout/");
+              String message = response["message"];
+              if (context.mounted) {
+                  if (response['status']) {
+                      String uname = response["username"];
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          content: Text("$message Sampai jumpa, $uname."),
+                      ));
+                      Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(builder: (context) => const LoginPage()),
+                      );
+                  } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                              content: Text(message),
+                          ),
+                      );
+                  }
+              }
+          }
         },
         child: Container(
           padding: const EdgeInsets.all(8),
